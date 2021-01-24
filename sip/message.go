232650008618c0abd5b7f5813c29df8900a3b1fe
dwdcth <dwdcth@163.com>
2021-01-24 11:10:01 +0800

    Update message.go
    宇视平台设备 xml body 换行修复

--- sip/message.go
+++ sip/message.go
@@ -219,10 +219,13 @@ func Decode(data []byte) (msg *Message, err error) {
 	}
 
 	headStr := strings.TrimSpace(msgArr[0])
-	if len(msgArr) > 1 {
-		msg.Body = strings.TrimSpace(msgArr[1])
+	msgArrLen := len(msgArr)
+	if msgArrLen > 1 {
+		for i := 1; i < msgArrLen; i++ {
+			msg.Body += strings.TrimSpace(msgArr[i])
+		}
 	}
-
+	
 	headStr = strings.Trim(headStr, CRLF)
 	headArr := strings.Split(headStr, CRLF)
 	for i, line := range headArr {
